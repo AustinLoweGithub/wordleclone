0fd9de3070e478c42cbbc5413d415190707bbc8d
AustinLoweGithub <81576463+AustinLoweGithub@users.noreply.github.com>
2022-03-19 20:48:35 -0400

    initial commit on wordleclone built with react

---
+++ src/Words.js
@@ -0,0 +1,29 @@
+import wordBank from "./wordbank.txt";
+
+export const boardDefault = [
+["","","","",""],
+["","","","",""],
+["","","","",""],
+["","","","",""],
+["","","","",""],
+["","","","",""]
+];
+
+
+export const generateWordSet = async () => {
+
+    let wordSet;
+    let targetWord;
+
+    await fetch(wordBank).then((response) => response.text()).then((result) => {
+    const wordArr = result.split("\n");
+    targetWord = wordArr[Math.floor(Math.random() * wordArr.length)]
+    wordSet = new Set(wordArr);
+   
+});
+
+return {wordSet, targetWord};
+
+
+
+};
